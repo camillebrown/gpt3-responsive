83a8309c39cccac56283252f9e4fc993a8b6bf33
Camille Brown <cbrown@creditkey.com>
2021-12-07 17:40:52 -0800

    update to navbar and header

---
+++ src/components/navbar/Menu.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default function Menu() {
+  return (
+    <>
+      <p>
+        <a href="#home">Home</a>
+      </p>
+      <p>
+        <a href="#wgpt3">What is GPT3?</a>
+      </p>
+      <p>
+        <a href="#possibility">Open AI</a>
+      </p>
+      <p>
+        <a href="#features">Case Studies</a>
+      </p>
+      <p>
+        <a href="#blog">Library</a>
+      </p>
+    </>
+  );
+}
